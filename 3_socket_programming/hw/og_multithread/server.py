fc997bc2e314a47d85a952d363528d7c95feeeb0
lmfaolmaolol <vangsur68@gmail.com>
2025-10-16 19:33:40 +0530

    improved socket programming

--- 3_socket_programming/hw/og_multithread/server.py
+++ 3_socket_programming/hw/og_multithread/server.py
@@ -2,21 +2,20 @@ import socket
 import threading
 
 #we use a try catch block to gracefully handle an error, without it the necessary clean up code conn.close() might not run causing leaks or incase our worker thread had acquired a lock on a resource in case of a try catch block we can release the lock in finally to avoid deadlocks, we can also add code in except block to roll back change and prevent corrupted data from cauing more errors
-def handle_client(conn,addr):
+def handle_client(conn, addr):
   try:
     print(f"got conn from client: {addr}")
     while True: 
       data = conn.recv(1024)
       if not data:
-        print("Nice talking to you. Bye!!")
+        print(f"Client {addr} disconnected")
         break
-    print(f"Data from client{addr} = {data.decode()}")
-  except ConnectionResetError:
-    print(f"Client {addr} disconnected forcefully")#incase the client connection terminates abruptly like in case of a keyboard interrupt, closing the terminal etc, it is a very common error
+      print(f"Data from client {addr} = {data.decode()}")
   except Exception as e:
-    print(f"an error: {e} occured with client {addr}")#to catch all other errors
+    print(f"Error with client {addr}: {e}")
   finally:
     conn.close()
+    print(f"Connection closed for {addr}")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket created")
