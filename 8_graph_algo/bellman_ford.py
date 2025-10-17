5f560250580fbfe583217d240bc3746e904fb601
lmfaolmaolol <vangsur68@gmail.com>
2025-10-17 11:54:27 +0530

    updated bellman

--- 8_graph_algo/bellman_ford.py
+++ 8_graph_algo/bellman_ford.py
@@ -16,12 +16,18 @@ def bellman_ford(graph, start):
         if new_distance<distances[neighbour]:
           distances[neighbour]=new_distance
 
-  negative_cycle = False
-  for vertex in graph:
+  for _ in range(len(graph)):
+    for vertex in graph:
       for neighbour, weight in graph[vertex]:
         new_distance = distances[vertex]+weight
 
         if new_distance<distances[neighbour]:
-          negative_cycle=True
+          distances[neighbour]=-math.inf
+
+  for vertex in distances:
+    if distances[vertex]==math.inf:
+      distances[vertex]=-1
+    elif distances[vertex]==-math.inf:
+      distances[vertex]=100000000
   
-  return distances, negative_cycle
+  return distances
